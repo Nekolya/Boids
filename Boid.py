4a4568faffe6812f1a71dd8b2e518aee07bac1c4
Olya Bykovskaya <olya75825@gmail.com>
2019-09-28 22:32:54 +0300

    Update

--- Boid.py
+++ Boid.py
@@ -1,29 +1,189 @@
 import random
+import pygame
 import time
+import math
 
 
 class Boid:
     def __init__(self):
         self.x = random.randint(10, 1100)
         self.y = random.randint(10, 600)
-        self.dx = random.randint(10, 20)/50*random.choice([-1, 1])
-        self.dy = random.randint(10, 20)/50*random.choice([-1, 1])
+        self.x_center = self.x + 13
+        self.y_center = self.y - 13
+
+        self.dx = random.randint(15, 20)/50*random.choice([-1, 1])
+        self.dy = random.randint(15, 20)/50*random.choice([-1, 1])
+        self.rect = pygame.Rect(self.x-15, self.y+15, 55, 55)
+
+        self.avoid_dx = 0
+        self.avoid_dy = 0
+        self.aligm_dx = self.dx
+        self.aligm_dy = self.dy
+        self.cohes_dx = 0
+        self.cohes_dy = 0
+
+        self.avoid_rect = pygame.Rect(self.x-7, self.y + 7, 39, 39)
+        self.co_rect = pygame.Rect(self.x-30, self.y+30, 85, 85)
+
+        self.neighbors = 1;
+        self.cohesion_x = self.x;
+        self.cohesion_y = self.y;
+        self.angle = math.atan(self.dy/self.dx)*180/math.pi
 
     def move(self):
+
+        self.cohesion()
+
+        #print("%.3f" % self.dx, "%.3f" % self.avoid_dx, "%.3f" % self.aligm_dx, "%.3f" % self.cohes_dx, sep="\t")
+
+        self.dx = (self.dx*4 + self.avoid_dx + self.aligm_dx/3 + self.cohes_dx/3)/2
+        self.dy = (self.dy*4 + self.avoid_dy + self.aligm_dy/3 + self.cohes_dy/3)/2
+        self.speed_limit()
+
+
+
         self.x += self.dx
         self.y += self.dy
-#    def change_
+        self.update()
 
-    def teleport(self, w, h):
-        if self.x > w:
-            self.x = -22
+#    def change_a
+    def update(self):
+        self.angle = math.atan(self.dy / self.dx)*180/math.pi
+        if self.dy > 0 and self.dx < 0:
+            self.angle += 180
+        if self.dy > 0 and self.dx > 0:
+            self.angle += 180
 
-        elif self.y > h:
-            self.y = -22
+        self.neighbors = 0;
+        self.cohesion_x = 0;
+        self.cohesion_y = 0;
+        self.avoid_dx = self.dx
+        self.avoid_dy = self.dy
+        self.aligm_dx = self.dx
+        self.aligm_dy = self.dy
+        self.cohes_dx = 0
+        self.cohes_dy = 0
 
-        elif self.x < -22:
-            self.x = w
+        self.x_center = self.x + 13
+        self.y_center = self.y - 13
 
-        elif self.y < -22:
-            self.y = h
+        self.rect.x = self.x - 15
+        self.rect.y = self.y + 15
+        self.avoid_rect.x = self.x - 7
+        self.avoid_rect.y = self.y + 7
+        self.co_rect.x = self.x - 30
+        self.co_rect.y = self.y + 30
+
+    def d_lim(self, speed):
+        lim = 1
+        while speed > lim or speed < -lim:
+            speed /= 1.2
+        return speed
+
+    def speed_limit(self):
+        lim = 0.7
+        const = 0.16
+
+        while self.dx > lim or self.dy > lim or self.dx < -lim or self.dy < -lim:
+            self.dx /= 1.2
+            self.dy /= 1.2
+            """
+
+            if self.dx > lim:
+                self.dx -= const
+
+            elif self.dx < -lim:
+                self.dx += const
+
+            if self.dy > lim:
+                self.dy -= const
+
+            elif self.dy < -lim:
+                self.dy += const"""
+
+    def walls(self, w, h):
+        mode = 2
+        if mode == 2:
+            if self.x > w:
+                self.dx = -22
+
+            elif self.y > h:
+                self.y = -22
+
+            elif self.x < -22:
+                self.x = w
+
+            elif self.y < -22:
+                self.y = h
+        elif mode:
+            if self.x > w or self.x < 0:
+                self.dx = -self.dx
+
+            elif self.y < 0 or self.y > h:
+                self.dy = -self.dy
+        else:
+            const = 0.4
+            if self.x > w - 30:
+                self.dx -= const
+
+            elif self.y > h - 30:
+                self.dy -= const
+
+            elif self.x < 30:
+                self.dx += const
+
+            elif self.y < 30:
+                self.dy += const
+
+    def avoid(self, boid):
+        if self.avoid_rect.colliderect(boid.avoid_rect):
+            #print(self.x_center, self.y_center, 'collide with', boid.x_center, boid.y_center)
+            const = 0.2
+            if self.x_center > boid.x_center:
+                self.avoid_dx += const
+            else:
+                self.avoid_dx -= const
+
+            if self.y_center > boid.y_center:
+                self.avoid_dy += const
+            else:
+                self.avoid_dy -= const
+
+            #self.avoid_dx = self.d_lim(self.avoid_dx)
+            #self.avoid_dy = self.d_lim(self.avoid_dy)
+
+    def alignment_accel(self, boid):
+        if self.rect.colliderect(boid.rect):
+            #res_dx = self.dx + boid.dx
+            #res_dy = self.dy + boid.dy
+            const = 1
+
+            self.aligm_dx = self.d_lim(boid.aligm_dx/const + self.aligm_dx)
+            self.aligm_dy = self.d_lim(boid.aligm_dy/const + self.aligm_dy)
+
+            boid.aligm_dx = self.d_lim(self.aligm_dx/const + boid.aligm_dx)
+            boid.aligm_dy = self.d_lim(self.aligm_dy/const + boid.aligm_dy)
+
+    def center_of_gravity(self, boid):
+        if self.co_rect.colliderect(boid.co_rect):
+            self.cohesion_x += boid.x
+            self.cohesion_y += boid.y
+
+            self.neighbors += 1
+
+    def cohesion(self):
+        if self.neighbors > 2:
+            x = self.cohesion_x/self.neighbors
+            y = self.cohesion_y/self.neighbors
+
+            dx = x - self.x
+            dy = y - self.y
+
+            const = 3
+            self.cohes_dx = self.d_lim(dx / const)
+            self.cohes_dy = self.d_lim(dy / const)
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # здесь подключаются модули
 import pygame
 from Boid import Boid
+import time
  
 # здесь определяются константы, классы и функции
-FPS = 100
-W = 1200
-H = 640
+FPS = 120
+W = 1300
+H = 680
+lenght = 120
 boids = []
-for b in range(100):
+for b in range(lenght):
     boids.append(Boid())
  
 # здесь происходит инициация, создание объектов и др.
@@ -16,29 +18,42 @@ win = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Boids")
 
 clock = pygame.time.Clock()
- 
+
 # если надо до цикла отобразить объекты на экране
 pygame.display.update()
-sprite = pygame.image.load("img.png")
+sprite = pygame.image.load("fish.png")
+#sprite = pygame.transform.scale(sprite, (13, 20))
+sprite = pygame.transform.scale(sprite, (15, 25))
+back = pygame.image.load("backwater.jpg")
+back = pygame.transform.scale(back, (W, H))
+checker = 0
+
 
 # главный цикл
 while True:
  
     # задержка
     clock.tick(FPS)
-
+    checker += 1
+    if checker > 100:
+        checker = 0
     # цикл обработки событий
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
-    win.fill((0, 0, 0))
-    # --------
-    # изменение объектов и многое др.
+    win.fill((0, 140, 160))
+    #win.blit(back, (0, 0))
+
     for boid in boids:
-        win.blit(sprite, (boid.x, boid.y))
+        win.blit(pygame.transform.rotate(sprite, boid.angle), (boid.x, boid.y))
+        if checker % 10 == 0:
+            for b in boids:
+                if b != boid:
+                    b.alignment_accel(boid)
+                    b.avoid(boid)
+                    b.center_of_gravity(boid)
         boid.move()
-        boid.teleport(W, H)
+        boid.walls(W, H)
     # --------
- 
     # обновление экрана
     pygame.display.update()
