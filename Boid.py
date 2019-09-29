2457ca18fdf3ca53b9f7472b6f710ae046cf3bcb
Olya Bykovskaya <olya75825@gmail.com>
2019-09-30 00:21:59 +0300

    Visual

--- Boid.py
+++ Boid.py
@@ -5,9 +5,9 @@ import math
 
 
 class Boid:
-    def __init__(self):
-        self.x = random.randint(10, 1100)
-        self.y = random.randint(10, 600)
+    def __init__(self, W, H):
+        self.x = random.randint(20, W - 20)
+        self.y = random.randint(20, H - 20)
         self.x_center = self.x + 13
         self.y_center = self.y - 13
 
@@ -23,7 +23,8 @@ class Boid:
         self.cohes_dy = 0
 
         self.avoid_rect = pygame.Rect(self.x-7, self.y + 7, 39, 39)
-        self.co_rect = pygame.Rect(self.x-30, self.y+30, 85, 85)
+        #self.co_rect = pygame.Rect(self.x-30, self.y+30, 85, 85)
+        self.co_rect = pygame.Rect(self.x-25, self.y+25, 75, 75)
 
         self.neighbors = 1;
         self.cohesion_x = self.x;
@@ -36,11 +37,9 @@ class Boid:
 
         #print("%.3f" % self.dx, "%.3f" % self.avoid_dx, "%.3f" % self.aligm_dx, "%.3f" % self.cohes_dx, sep="\t")
 
-        self.dx = (self.dx*4 + self.avoid_dx + self.aligm_dx/3 + self.cohes_dx/3)/2
-        self.dy = (self.dy*4 + self.avoid_dy + self.aligm_dy/3 + self.cohes_dy/3)/2
+        self.dx = (self.dx*5 + self.avoid_dx + self.aligm_dx/3 + self.cohes_dx/3)/2
+        self.dy = (self.dy*5 + self.avoid_dy + self.aligm_dy/3 + self.cohes_dy/3)/2
         self.speed_limit()
-
-
 
         self.x += self.dx
         self.y += self.dy
@@ -71,8 +70,8 @@ class Boid:
         self.rect.y = self.y + 15
         self.avoid_rect.x = self.x - 7
         self.avoid_rect.y = self.y + 7
-        self.co_rect.x = self.x - 30
-        self.co_rect.y = self.y + 30
+        self.co_rect.x = self.x - 25
+        self.co_rect.y = self.y + 25
 
     def d_lim(self, speed):
         lim = 1
@@ -116,11 +115,18 @@ class Boid:
             elif self.y < -22:
                 self.y = h
         elif mode:
-            if self.x > w or self.x < 0:
-                self.dx = -self.dx
+            const = 0.03
+            if self.x > w:
+                self.dx = -22
 
-            elif self.y < 0 or self.y > h:
-                self.dy = -self.dy
+            elif self.x < -22:
+                self.x = w
+
+            elif self.y > h - 100:
+                self.dy -= const
+
+            elif self.y < 100:
+                self.dy += const
         else:
             const = 0.4
             if self.x > w - 30:
@@ -172,16 +178,20 @@ class Boid:
             self.neighbors += 1
 
     def cohesion(self):
-        if self.neighbors > 2:
+        if self.neighbors > 6:
             x = self.cohesion_x/self.neighbors
             y = self.cohesion_y/self.neighbors
 
             dx = x - self.x
             dy = y - self.y
 
-            const = 3
+            const = 2.2
             self.cohes_dx = self.d_lim(dx / const)
             self.cohes_dy = self.d_lim(dy / const)
+
+        else:
+            self.dx *= 0.18
+            self.dy *= 0.18
 
 
 
--- main.py
+++ main.py
@@ -4,28 +4,38 @@ from Boid import Boid
 import time
  
 # здесь определяются константы, классы и функции
-FPS = 120
-W = 1300
-H = 680
+FPS = 200
+W = 1024
+H = 600
 lenght = 120
 boids = []
 for b in range(lenght):
-    boids.append(Boid())
+    boids.append(Boid(W, H))
  
 # здесь происходит инициация, создание объектов и др.
 pygame.init()
 win = pygame.display.set_mode((W, H))
-pygame.display.set_caption("Boids")
+pygame.display.set_icon(pygame.image.load("icon.png"))
+
+pygame.display.set_caption("Coral fishes")
 
 clock = pygame.time.Clock()
 
 # если надо до цикла отобразить объекты на экране
 pygame.display.update()
 sprite = pygame.image.load("fish.png")
+sprite2 = pygame.image.load("fish2.png")
+sprite3 = pygame.image.load("fish3.png")
+sprite4 = pygame.image.load("fish4.png")
+sprite5 = pygame.image.load("fish5.png")
+seaweed = pygame.transform.scale(pygame.image.load("seaweed.png"), (70, 310))
+corals = pygame.transform.scale(pygame.image.load("coral_reef.png"), (W+20, 220))
+
 #sprite = pygame.transform.scale(sprite, (13, 20))
-sprite = pygame.transform.scale(sprite, (15, 25))
-back = pygame.image.load("backwater.jpg")
-back = pygame.transform.scale(back, (W, H))
+#sprite = pygame.transform.scale(sprite, (15, 25))
+#sprite = pygame.transform.scale(sprite, (13, 20))
+#back = pygame.image.load("backwater.jpg")
+#back = pygame.transform.scale(back, (W, H))
 checker = 0
 
 
@@ -41,11 +51,31 @@ while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
-    win.fill((0, 140, 160))
-    #win.blit(back, (0, 0))
+    win.fill((40, 170, 210))
+    pygame.draw.rect(win, (35, 165, 205), (0, 500, 1200, 600))
+    pygame.draw.rect(win, (30, 160, 200), (0, 530, 1200, 600))
+    pygame.draw.rect(win, (40, 155, 190), (0, 560, 1200, 600))
+    pygame.draw.rect(win, (30, 150, 180), (0, 580, 1200, 600))
+    win.blit(corals, (-20, 410))
+    pygame.draw.circle(win, (55, 175, 220), (-100, - 600), 1000)
+    pygame.draw.circle(win, (60, 185, 230), (-100, - 600), 800)
+    pygame.draw.circle(win, (80, 215, 230), (-100, - 600), 700)
+    pygame.draw.circle(win, (180, 240, 250), (-100, - 600), 630)
 
+
+    counter = 0
     for boid in boids:
-        win.blit(pygame.transform.rotate(sprite, boid.angle), (boid.x, boid.y))
+        counter += 1
+        if counter % 5 == 0:
+            win.blit(pygame.transform.rotate(sprite2, boid.angle), (boid.x, boid.y))
+        elif counter % 6 == 0:
+            win.blit(pygame.transform.rotate(sprite3, boid.angle), (boid.x, boid.y))
+        elif counter % 4 == 0:
+            win.blit(pygame.transform.rotate(sprite4, boid.angle), (boid.x, boid.y))
+        elif counter % 7 == 0:
+            win.blit(pygame.transform.rotate(sprite5, boid.angle), (boid.x, boid.y))
+        else:
+            win.blit(pygame.transform.rotate(sprite, boid.angle), (boid.x, boid.y))
         if checker % 10 == 0:
             for b in boids:
                 if b != boid:
@@ -56,4 +86,5 @@ while True:
         boid.walls(W, H)
     # --------
     # обновление экрана
+    #win.blit(seaweed, (120, 300))
     pygame.display.update()
